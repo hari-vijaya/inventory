6f50895ffac790ce6da9939ef91cebb3b72db1f4
harimurug <harimurugan@yellowowl.app>
2024-01-31 21:34:26 +0530

    system print branch b 1

--- src/main/java/com/codebasicz/inventory/controller/ProductsController.java
+++ src/main/java/com/codebasicz/inventory/controller/ProductsController.java
@@ -28,7 +28,7 @@ public class ProductsController {
     public ModelAndView getProducts(@RequestParam(value = "search", required = false) String search,
                                     @RequestParam(value = "pageNo", defaultValue = "1") Integer pageNo,
                                     @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
-        System.out.print("test1");
+        System.out.print("test2");
         Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
         ModelAndView modelAndView = new ModelAndView("products/list");
         Page<Product> productPage = productsService.getProducts(search, pageable);
